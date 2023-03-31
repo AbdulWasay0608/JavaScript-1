088cfe7fe1dbb7208302eea7e3c15a165e2146fc
AbdulWasay0608 <abdulwasay0608@gmail.com>
2023-03-31 23:53:29 +0500

    Work complete

---
+++ app.js
@@ -0,0 +1,128 @@
+//=====ALERT=====//
+
+alert("Thanks for your input!");
+
+//=====Variables for Strings=====//
+
+var name = "Mark";
+name = "Ace";
+
+alert(name);
+
+var nationality;
+nationality = "U.S.";
+
+alert(nationality);
+
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+
+alert(lastName, cityOfOrigin, aussieGreeting)
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+
+alert(newNum)
+
+
+var originalNum1 = 30;
+var numToBeAdded = 15;
+var newNum1 = originalNum1 + numToBeAdded;
+
+alert(newNum1)
+
+
+var originalNumber2 = 30;
+var newNumber2 = originalNumber2 + 7;
+
+alert(newNumber2)
+
+
+alert(2 + 2);
+// var popularNumber = 2 + 2;
+// var popularNumber = 4;
+
+
+
+var popularNumber = 12 - 24;
+alert(popularNumber)
+
+
+
+var popularNumber = 3 * 12;
+alert(popularNumber)
+
+
+var num = 10;
+var popularNumber = num + 200;
+
+alert(popularNumber)
+
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+
+alert(popularNumber)
+
+
+var num = 2;
+var newNum = num++;
+
+alert(newNum)
+
+
+var num = 2;
+var newNum = ++num;
+
+alert(newNum)
+
+
+var num = 2;
+var newNum = num--;
+
+alert(newNum)
+
+
+var num = 2;
+var newNum = --num;
+
+alert(newNum)
+
+
+
+var resultOfComputation = (2 * 4) * 4 + 2;
+
+alert(resultOfComputation)
+
+
+var totalCost = (1 + 3) * 4;
+
+alert(totalCost)
+
+
+
+var message = "Thanks, ";
+var userName = "Abdul Wasay";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+
+
+alert("2" + "2");
+
+alert("2 plus 2 equals " + 2 + 2);
+
+
+
+
+var question = "Write Your Country?";
+var defaultAnswer = "Pakistan";
+var spec = prompt(question, defaultAnswer);
+
+alert(spec)
